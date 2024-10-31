2ca52820c398bcaa567225be06de0477de1c0751
nimra-Ashfaq <rananimraashfaq007@gmail.com>
2024-10-31 09:57:17 +0500

    mywork

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+
+import Placeorder from './pages/PlaceOrder/Placeorder'
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+    <Routes>
+      <Route path='/Placeorder' element={<Placeorder/>}></Route>
+    </Routes>    </>
+  
+  )
 }
 
-export default App;
+export default App
